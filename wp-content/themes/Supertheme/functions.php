cdc79fa7d6e7713552b62ed1e10672f27e0bd46c
Robert M. Parker <rparker@yamiko.ninja>
2016-12-01 14:02:34 -0800

    page builder fields

--- wp-content/themes/Supertheme/functions.php
+++ wp-content/themes/Supertheme/functions.php
@@ -2,9 +2,18 @@
 require_once __DIR__.'/app/bootstrap.php';
 require_once __DIR__.'/src/functions.php';
 
+// timber stuff
 add_filter('timber/context', function($data){
     // logos
     $data['menu'] = new Timber\Menu('primary_menu');
 
     return $data;
 });
+
+// register some acf fields
+if(function_exists('acf_add_local_field_group')){
+    $parser = new \Symfony\Component\Yaml\Parser();
+    // header
+    $fields = $parser->parse(file_get_contents(__DIR__.'/app/config/page_builder.yml'));
+    acf_add_local_field_group($fields);
+}
