5ab40df7e4667befd5fbd61122278127317f8fd0
Robert M. Parker <rparker@yamiko.ninja>
2016-12-03 17:22:55 -0800

    generic header

--- wp-content/themes/Supertheme/functions.php
+++ wp-content/themes/Supertheme/functions.php
@@ -4,9 +4,11 @@ require_once __DIR__.'/src/functions.php';
 
 // timber stuff
 add_filter('timber/context', function($data){
-    // logos
+    // menu
     $data['menu'] = new Timber\Menu('primary_menu');
-
+    // header background
+    $data['default_header_background'] = get_field('default_header_background', 'option');
+    
     return $data;
 });
 
@@ -14,6 +16,9 @@ add_filter('timber/context', function($data){
 if(function_exists('acf_add_local_field_group')){
     $parser = new \Symfony\Component\Yaml\Parser();
     // header
+    $fields = $parser->parse(file_get_contents(__DIR__.'/app/config/header.yml'));
+    acf_add_local_field_group($fields);
+    // page builder
     $fields = $parser->parse(file_get_contents(__DIR__.'/app/config/page_builder.yml'));
     acf_add_local_field_group($fields);
 }
