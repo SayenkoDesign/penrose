cb433d05fd6e80525444bef4f62ddb7ed3a5b24e
Robert M. Parker <rparker@yamiko.ninja>
2016-12-05 02:25:31 -0800

    footer

--- wp-content/themes/Supertheme/functions.php
+++ wp-content/themes/Supertheme/functions.php
@@ -8,7 +8,20 @@ add_filter('timber/context', function($data){
     $data['menu'] = new Timber\Menu('primary_menu');
     // header background
     $data['default_header_background'] = get_field('default_header_background', 'option');
-    
+    // footer
+    $data['facebook'] = get_field('facebook', 'option');
+    $data['instagram'] = get_field('instagram', 'option');
+    $data['pinterest'] = get_field('pinterest', 'option');
+    $data['google_plus'] = get_field('google_plus', 'option');
+    $data['contact_form_id'] = get_field('contact_form_id', 'option');
+    $data['phone'] = get_field('phone', 'option');
+    $data['phone_url'] = 'tel:'.preg_replace('/[^0-9]/', '', get_field('phone', 'option'));
+    $data['address_1'] = get_field('address_1', 'option');
+    $data['address_2'] = get_field('address_2', 'option');
+    $data['directions'] = get_field('directions_link', 'option');
+    $data['hours'] = get_field('hours', 'option');
+    $data['copyright'] = get_field('copyright', 'option');
+
     return $data;
 });
 
