af3cbddf65d8bcddb5da957f1f89a06ea728b767
Robert M. Parker <rparker@yamiko.ninja>
2016-12-08 15:33:32 -0800

    insurance partners

--- wp-content/themes/Supertheme/web/scripts/app.js
+++ wp-content/themes/Supertheme/web/scripts/app.js
@@ -16,4 +16,18 @@ jQuery(function() {
         }
     });
     jQuery('.slick').slick();
+    jQuery('.partners-slider').slick({
+        arrows: true,
+        slidesToShow: 4,
+        responsive: [{
+            breakpoint: 700,
+            settings: {
+                slidesToShow: 1
+            },
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
 });
