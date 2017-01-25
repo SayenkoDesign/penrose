737f092ba135659618debb282e9c3a284c3a3e27
Robert M. Parker <rparker@yamiko.ninja>
2017-01-25 13:54:15 -0800

    pagination and looping through success stories

--- wp-content/themes/Supertheme/archive.php
+++ wp-content/themes/Supertheme/archive.php
@@ -31,5 +31,26 @@ if(get_post_type() == "case_study") {
 }
 array_unshift($templates, 'archive-'.get_post_type().'.html.twig' );
 
+
+$max_pages = $GLOBALS['wp_query']->max_num_pages;
+$pagination = '';
+if ($max_pages > 1) {
+    $current_page = get_query_var('paged') ?: 1;
+    $pagination .= '<ul class="pagination text-center" role="navigation" aria-label="Pagination">';
+    if ($current_page > 1) {
+        $pagination .= '<li class="pagination-previous"><a href="' . get_pagenum_link($current_page-1) . '">Previous <span class="show-for-sr">page</span></a></li>';
+    }
+    for ($page=1; $page <= $max_pages; $page++) {
+        $page_link = get_pagenum_link($page);
+        $current = $current_page == $page ? 'class="current"' : '';
+        $pagination .= '<li><a href="'.$page_link.'" aria-label="Page '.$page.'" '.$current.'>'.$page.'</a></li>';
+    }
+    if ($current_page < $max_pages) {
+        $pagination .= '<li class="pagination-next"><a href="' . get_pagenum_link($current_page+1) . '">Next <span class="show-for-sr">page</span></a></li>';
+    }
+    $pagination .= '</ul>';
+}
+$context['pagination'] = $pagination;
+
 $context['posts'] = Timber::get_posts();
 $timber::render($templates, $context);
--- wp-content/themes/Supertheme/single.php
+++ wp-content/themes/Supertheme/single.php
@@ -9,6 +9,29 @@ $context = Timber::get_context();
 $post = new Post();
 $context['post'] = $post;
 $context["acf"] = get_field_objects($context["post"]->ID);
+
+if(get_post_type() == 'case_study') {
+    $posts_array = get_posts([
+        'numberposts'     => 1,
+        'offset'          => 0,
+        'orderby'         => 'post_date',
+        'order'           => 'ASC',
+        'post_type'       => 'case_study',
+        'post_status'     => 'publish'
+    ]);
+    $context['first_post_link'] = get_permalink($posts_array[0]);
+
+    $posts_array = get_posts([
+        'numberposts'     => 1,
+        'offset'          => 0,
+        'orderby'         => 'post_date',
+        'order'           => 'DESC',
+        'post_type'       => 'case_study',
+        'post_status'     => 'publish'
+    ]);
+    $context['last_post_link'] = get_permalink($posts_array[0]);
+}
+
 if (post_password_required($post->ID)) {
     Timber::render('single-password.html.twig', $context);
 } else {
