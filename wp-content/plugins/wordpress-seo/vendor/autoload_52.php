d789861e7d4e32a67b4f0f895a6ea45f6d9a3292
Robert Parker <rparker@kodama.design>
2017-07-14 02:42:15 -0700

    live updates

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit150a5c7d27b21cbf63d20b632acec1b7::getLoader();
+return ComposerAutoloaderInit85ff0426e49f198c57f62ac90c904ecd::getLoader();
